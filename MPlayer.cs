e918c1152d9d8a6a5e865f463718f4269dec0424
RZOK <47612683+RZOK@users.noreply.github.com>
2019-11-10 17:55:20 -0500

    Update MPlayer.cs

--- MPlayer.cs
+++ MPlayer.cs
@@ -1,20 +1,5 @@
 using Terraria.DataStructures;
 using Terraria.ModLoader;
-using System.Runtime.InteropServices;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Terraria;
-using Terraria.Graphics.Effects;
-using Terraria.Graphics.Shaders;
-using Terraria.ID;
-using Terraria.Initializers;
-using Terraria.IO;
-using Terraria.GameContent;
-using BSODOnDeath;
 
 namespace BSODOnDeath
 {
